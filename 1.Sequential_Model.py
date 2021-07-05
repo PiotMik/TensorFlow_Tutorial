1207046aa30b5ea34b794e57a5f3362716f261c9
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2021-07-05 03:41:31 +0200

    Hello TensorFlow!
    Signed-off-by: PIOTR MIKLER <piotr.mikler1997@gmail.com>

--- 1.Sequential_Model.py
+++ 1.Sequential_Model.py
@@ -1,7 +1,23 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import (Dense, Flatten)
 
 if __name__ == '__main__':
-    model = Sequential(layers=[Dense(64, activation='relu'),
-                               Dense(10, activation='softmax')
-                               ])
+    # Initialization of model
+    model1 = Sequential(layers=[Dense(64, activation='relu'),
+                                Dense(10, activation='softmax')
+                                ])
+
+    # The same as above,
+    model2 = Sequential()
+    model2.add(Dense(64, activation='relu'))
+    model2.add(Dense(10, activation='softmax'))
+
+    # You can tell at the beginning what is the data dimension
+    model3 = Sequential(layers=[Dense(64, activation='relu', input_shape=(784,)),
+                                Dense(10, activation='softmax')
+                                ])
+
+    # Flatten dimensions before sending to the first layer
+    model4 = Sequential([Flatten(input_shape= (28, 28)),
+                         Dense(64, activation='relu'),
+                         Dense(10, aactivation='softmax')])
