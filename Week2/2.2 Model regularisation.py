2f863f8476e0d1289f2765a68b8f9d9cb8918505
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2021-07-19 01:02:04 +0200

    Regularizers
    Signed-off-by: PIOTR MIKLER <piotr.mikler1997@gmail.com>

---
+++ Week2/2.2 Model regularisation.py
@@ -0,0 +1,44 @@
+from tensorflow.python.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras import regularizers
+from sklearn.datasets import load_diabetes
+import matplotlib.pyplot as plt
+
+diabetes_dataset = load_diabetes()
+data = diabetes_dataset['data']
+targets = diabetes_dataset['target']
+targets = (targets - targets.mean(axis=0)) / targets.std(axis=0)
+
+train_data, test_data, train_targets, test_targets = train_test_split(data, targets, test_size=0.1)
+
+def get_regularized_model(wd, rate):
+    model = Sequential([
+        Dense(128, activation='relu', kernel_regularizer=regularizers.l2(wd),
+              input_shape=(train_data.shape[1],)),
+        Dropout(rate),
+        Dense(128, kernel_regularizer=regularizers.l2(wd), activation='relu'),
+        Dropout(rate),
+        Dense(128, kernel_regularizer=regularizers.l2(wd), activation='relu'),
+        Dropout(rate),
+        Dense(128, kernel_regularizer=regularizers.l2(wd), activation='relu'),
+        Dropout(rate),
+        Dense(128, kernel_regularizer=regularizers.l2(wd), activation='relu'),
+        Dropout(rate),
+        Dense(128, kernel_regularizer=regularizers.l2(wd), activation='relu'),
+        Dropout(rate),
+        Dense(1)
+    ])
+    return model
+model = get_regularized_model(1e-5, 0.1)
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+history = model.fit(train_data, train_targets, epochs=100,
+                    validation_split=0.15, batch_size=64, verbose=False)
+model.evaluate(test_data, test_targets, verbose=2)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss vs epochs')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Training', 'Validation'], loc='upper right')
+plt.show()
