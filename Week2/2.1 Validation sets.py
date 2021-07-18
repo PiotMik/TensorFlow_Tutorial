02406389027230f4442f946aaa9740340c66d15e
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2021-07-19 00:34:14 +0200

    Week 2 folder initialization
    Signed-off-by: PIOTR MIKLER <piotr.mikler1997@gmail.com>

---
+++ Week2/2.1 Validation sets.py
@@ -0,0 +1,43 @@
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+
+diabetes_dataset = load_diabetes()
+data = diabetes_dataset['data']
+targets = diabetes_dataset['target']
+targets = (targets - targets.mean(axis=0)) / targets.std(axis=0)
+
+train_data, test_data, train_targets, test_targets = train_test_split(data, targets, test_size=0.1)
+
+
+def get_model():
+    model = Sequential([
+        Dense(128, activation='relu', input_shape=(train_data.shape[1],)),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(1)
+    ])
+    return model
+
+
+model = get_model()
+model.summary()
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+history = model.fit(train_data, train_targets,
+                    epochs=100, validation_split=0.15,
+                    batch_size=64, verbose=False)
+
+model.evaluate(test_data, test_targets, verbose=2)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss vs epochs')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Training', 'Validation'], loc='upper right')
+plt.show()
